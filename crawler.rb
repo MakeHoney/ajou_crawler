6af3b540266b2ff7af6f0c56414e46b15363e97e
MakeHoney <makehoney3@gmail.com>
2018-04-02 01:43:40 +0900

    리드미 구획

--- crawler.rb
+++ crawler.rb
@@ -10,10 +10,11 @@ end
 
 f = File.open('test.txt', 'w')
 
-html = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp?date=2018-03-30').to_s
+html = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp').to_s
 # html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
 # f.puts html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'} --> method
 html = fix_url(html)
+f.puts html
 page = Nokogiri::HTML(html)
 page.css('table.ajou_table li').each do |li|
 	puts li.text
