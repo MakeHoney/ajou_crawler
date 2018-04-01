dbd25fe8b307d0cab86d02d00af99eb7648761da
MakeHoney <makehoney3@gmail.com>
2018-04-02 01:11:06 +0900

    전체 크롤링 이상 없음

--- crawler.rb
+++ crawler.rb
@@ -2,56 +2,19 @@ require 'http'
 require 'open-uri'
 require 'nokogiri'
 
-def fix_irregular_html(html)
-  regexp = /<([^<>]*)(<|$)/
-
-  #we need to do this multiple time as regex are overlapping
-  while (fixed_html = html.gsub(regexp, "&lt;\\1\\2")) && fixed_html != html
-    html = fixed_html
-  end
-
-  fixed_html
+def fix_url(html)
+	html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
+	html = html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'}
 end
-integratedUrl = ""
 
 
-url = 'http://www.ajou.ac.kr/kr/life/food.jsp?date=2018-03-30'
-open(url) {|f|
-  f.each_line {|line|
-  	si = line.gsub!('<', 'sibal')
-  	p si
-  	# p fix_irregular_html(line)
-  	# integratedUrl += line
-  }
-  # p f.base_uri         # <URI::HTTP:0x40e6ef2 URL:http://www.ruby-lang.org/en/>
-  # p f.content_type     # "text/html"
-  # p f.charset          # "iso-8859-1"
-  # p f.content_encoding # []
-  # p f.last_modified    # Thu Dec 05 02:45:02 UTC 2002
-}
-# url = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp?date=2018-03-30').to_s
-# abc = fix_irregular_html(url)
-# puts abc
-# # page = Nokogiri::HTML(url).to_xhtml(indent:3)
+f = File.open('test.txt', 'w')
 
-# page = Nokogiri::HTML(abc)
-
-# puts page
-# page.gsub!('&#13;', "")
-# page2 = page.gsub(/\s+/, "")
-# page3 = Nokogiri::HTML(page)
-# page3.css('table.ajou_table').each do |li|
-# 	puts li.text
-# 	puts '************************'
-# end
-# puts page3.css('table.ajou_table td')[10].text
-
-
-# page.css('table.ajou_table li').each do |li|
-# 	puts li.text
-# end
-
-
-# page3.css('table.ajou_table li').each do |li|
-# 	puts li.text
-# end
+html = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp?date=2018-03-30').to_s
+# html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
+# f.puts html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'} --> method
+html = fix_url(html)
+page2 = Nokogiri::HTML(html)
+page2.css('table.ajou_table li').each do |li|
+	puts li.text
+end
