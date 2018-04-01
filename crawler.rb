fdb639fe809d5ff84595a2072862cbcee6e7efa4
MakeHoney <makehoney3@gmail.com>
2018-04-02 07:44:00 +0900

    modulization

--- crawler.rb
+++ crawler.rb
@@ -2,20 +2,44 @@ require 'http'
 require 'open-uri'
 require 'nokogiri'
 
-def fix_url(html)
-	html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
-	html = html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'}
+	# f = File.open('test.txt', 'w')
+	# f.puts @html #	파일 입출력을 이용하여 문서 디버깅
+
+class SchoolFood
+	@html; @page
+
+	def initialize
+		@html = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp').to_s
+		@html = fixUrl(@html)
+		@page = Nokogiri::HTML(@html)
+	end
+
+	def fixUrl(html)
+		html.gsub!('no_right left_pd', 'no_right_left_pd')
+		html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
+		html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'}
+	end
+	
+	def studentFoodCourt
+		@page.css('table.ajou_table')[0].css('td.no_right_left_pd li').each do |li|
+		puts li.text
+		end	
+	end
+
+	def schoolFood
+		@page.css('table.ajou_table td.no_right_left_pd li').each do |td|
+			puts td.text
+		end
+	end
+
+	def wholeList
+		page.css('table.ajou_table li').each do |li|
+		puts li.text
+		end		
+	end
+
 end
 
+test = SchoolFood.new()
 
-f = File.open('test.txt', 'w')
-
-html = HTTP.get('http://www.ajou.ac.kr/kr/life/food.jsp').to_s
-# html.gsub!(/<[가-힣]/) {|s| s = '&lt;' + s[1]}
-# f.puts html.gsub!(/[가-힣]>/) {|s| s = s[0] + '&gt;'} --> method
-html = fix_url(html)
-f.puts html
-page = Nokogiri::HTML(html)
-page.css('table.ajou_table li').each do |li|
-	puts li.text
-end
+test.arrangeList
